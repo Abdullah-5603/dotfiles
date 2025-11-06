3bd38d84f9051987480280ff2fb3d34d9d39801c
PixelnRoot <70786208+pixelnroot@users.noreply.github.com>
2025-11-07 03:15:20 +0600

    Added the markdown viewer

---
+++ neovim/lua/plugins/markdown.lua
@@ -0,0 +1,16 @@
+return {
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && yarn install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+		vim.g.mkdp_auto_start = 0
+		vim.g.mkdp_auto_close = 0
+		vim.g.mkdp_refresh_slow = 1
+		vim.g.mkdp_browser = "brave"
+	end,
+	ft = { "markdown" },
+	config = function()
+		vim.keymap.set("n", "<leader>mp", ":MarkdownPreviewToggle<CR>", { desc = "Toggle Markdown Preview" })
+	end,
+}
