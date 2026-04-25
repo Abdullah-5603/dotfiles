40dab3a56befe900b5c94bce818dab758b513f4d
MD Abu Abdullah <abdullah72308@gmail.com>
2026-04-25 14:30:47 +0600

    Floaterm mapping key bindings updated

--- neovim/lua/plugins/floaterm.lua
+++ neovim/lua/plugins/floaterm.lua
@@ -1,7 +1,7 @@
 -- lua/plugins/floaterm.lua
 return {
 	"voldikss/vim-floaterm",
-	cmd = { "FloatermToggle", "FloatermNew" },
+	cmd = { "FloatermToggle", "FloatermNew", "FloatermNext", "FloatermPrev" },
 	init = function()
 		-- Window look
 		vim.g.floaterm_width = 0.7
@@ -26,12 +26,14 @@ return {
 
 		-- new terminal
 		map_bimode("<leader>tn", "<cmd>FloatermNew<CR>", "New terminal")
+		map_bimode("<leader>tj", "<cmd>FloatermPrev<CR>", "Previous terminal")
+		map_bimode("<leader>tk", "<cmd>FloatermNext<CR>", "Next terminal")
 
-		-- t1..t9 → go to Nth terminal (toggle if hidden)
+		-- t1..t9 → named terminal slots (always valid; create if missing)
 		for i = 1, 9 do
 			local lhs = string.format("<leader>t%d", i)
-			local rhs = string.format("<cmd>%dFloatermToggle<CR>", i)
-			map_bimode(lhs, rhs, string.format("Terminal #%d", i))
+			local rhs = string.format("<cmd>FloatermToggle term%d<CR>", i)
+			map_bimode(lhs, rhs, string.format("Terminal slot #%d", i))
 		end
 
 		return K
